66bd99270541bca35b9ad860fcce3321a884bb3e
ostrosco <ostrosco@fastmail.fm>
2018-01-09 23:16:00 -0700

    Renamed Easel struct to EaselCoords and made a new Easel struct
    - EaselCoords now holds the coordinates of the Easel, Easel now holds
      state information about the Easel and provides some helper functions
      for drawing on the easel in-game
    - added support for changing the brush size

--- src/easel.rs
+++ src/easel.rs
@@ -1,40 +1,97 @@
 extern crate failure;
 
+extern crate enigo;
 extern crate serde;
 extern crate serde_json;
 
 use std::io::{Read, Write};
 use std::fs::File;
+use std::time::Duration;
+use std::thread;
 use self::failure::Error;
+use self::enigo::{Enigo, MouseButton, MouseControllable};
 
 pub type Point = (i32, i32);
 
 #[derive(Serialize, Deserialize)]
-pub struct Easel {
+pub struct EaselCoords {
     pub portrait_bounds: (Point, Point),
     pub landscape_bounds: (Point, Point),
     pub paintbrush: Point,
     pub spray_can: Point,
     pub pen: Point,
+    pub decrease_brush: Point,
+    pub increase_brush: Point,
     pub change_orientation: Point,
     pub color_start: Point,
     pub color_row_step: i32,
     pub color_col_step: i32,
 }
 
-impl Easel {
-    pub fn new(path: String) -> Result<Easel, Error> {
+impl EaselCoords {
+    pub fn new(path: String) -> Result<EaselCoords, Error> {
         let mut file = File::open(path)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        let win: Easel = serde_json::from_str(&contents)?;
+        let win: EaselCoords = serde_json::from_str(&contents)?;
         Ok(win)
     }
 
-    pub fn save(&self, path: String) -> Result<(), Error> {
+    pub fn save(&self, path: &str) -> Result<(), Error> {
         let j = serde_json::to_string_pretty(self)?;
         let mut file = File::create(&path)?;
         file.write_all(j.as_bytes())?;
         Ok(())
+    }
+}
+
+pub enum Orientation {
+    Portrait,
+    Landscape,
+}
+
+const NUM_BRUSH_STEPS: i32 = 16;
+const STARTING_BRUSH: i32 = 9;
+
+pub struct Easel {
+    pub easel_coords: EaselCoords,
+    pub orientation: Orientation,
+    pub brush_size: i32,
+}
+
+impl Easel {
+    pub fn new(path: String) -> Result<Easel, Error> {
+        let easel_coords = EaselCoords::new(path)?;
+        let orientation = Orientation::Portrait;
+        Ok(Easel {
+            easel_coords: easel_coords,
+            orientation: orientation,
+            brush_size: STARTING_BRUSH,
+        })
+    }
+
+    pub fn change_orientation(
+        &mut self,
+        enigo: &mut Enigo,
+        wait_time: &Duration,
+    ) {
+        let orient_coords = self.easel_coords.change_orientation;
+        enigo.mouse_move_to(orient_coords.0, orient_coords.1);
+        thread::sleep(*wait_time);
+        enigo.mouse_down(MouseButton::Left);
+        thread::sleep(*wait_time);
+        enigo.mouse_up(MouseButton::Left);
+        thread::sleep(*wait_time);
+        self.orientation = match self.orientation {
+            Orientation::Portrait => Orientation::Landscape,
+            Orientation::Landscape => Orientation::Portrait,
+        };
+    }
+
+    pub fn get_bounds(&self) -> (Point, Point) {
+        match self.orientation {
+            Orientation::Portrait => self.easel_coords.portrait_bounds,
+            Orientation::Landscape => self.easel_coords.landscape_bounds,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#[macro_use]
-extern crate serde_derive;
 extern crate enigo;
 extern crate failure;
+#[macro_use]
+extern crate serde_derive;
 
 pub mod easel;
 
 use easel::Easel;
-use enigo::{Enigo, MouseControllable, MouseButton};
+use enigo::{Enigo, MouseButton, MouseControllable};
 use std::thread;
 use std::time::Duration;
 
@@ -25,25 +25,32 @@ fn main() {
     // we'll need to find a way to calculate the position of the easel relative
     // to the input resolution, but at the moment this works.
 
-    let easel = match Easel::new("coords.json".to_string()) {
+    let mut easel = match Easel::new("coords.json".to_string()) {
         Ok(e) => e,
         Err(e) => panic!("Error loading config: {}", e.cause()),
     };
-    
+
     // Assume that the game runs at 60Hz, we should refresh every 16ms. Hence,
     // let's wait two frames during each operation.
     let wait_time = Duration::from_millis(32);
     let mut enigo = Enigo::new();
-    let (ul, lr) = easel.portrait_bounds;
-    let (everglade_x, everglade_y) = (easel.color_start.0 + easel.color_col_step * 4,
-                                      easel.color_start.1);
-    let (pink_x, pink_y) = (easel.color_start.0 + easel.color_col_step * 2,
-                            easel.color_start.1 + easel.color_row_step * 2);
-    
+
+    easel.change_orientation(&mut enigo, &wait_time);
+    let (ul, lr) = easel.get_bounds();
+    let coords = easel.easel_coords;
+    let (everglade_x, everglade_y) = (
+        coords.color_start.0 + coords.color_col_step * 4,
+        coords.color_start.1,
+    );
+    let (pink_x, pink_y) = (
+        coords.color_start.0 + coords.color_col_step * 2,
+        coords.color_start.1 + coords.color_row_step * 2,
+    );
+
     // Let's first change the color to pink.
     change_color(&mut enigo, &wait_time, pink_x, pink_y);
 
-    // This should draw a black line across the left of the easel.
+    // This should draw a black line across the left of the coords.
     enigo.mouse_move_to(ul.0, ul.1);
     thread::sleep(wait_time);
     enigo.mouse_down(MouseButton::Left);
@@ -56,7 +63,7 @@ fn main() {
     // Now, change the color to everglade.
     change_color(&mut enigo, &wait_time, everglade_x, everglade_y);
 
-    // Now, draw an everglade line across the right of an easel.
+    // Now, draw an everglade line across the right of an coords.
     enigo.mouse_move_to(lr.0, ul.1);
     thread::sleep(wait_time);
     enigo.mouse_down(MouseButton::Left);
