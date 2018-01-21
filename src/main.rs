694f4ac743a43e91150f8365e678e7149f2fa948
ostrosco <ostrosco@fastmail.fm>
2018-01-21 15:31:23 -0700

    Removing hardcoded picture and fixing landscape coordinates

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,18 @@ pub mod easel;
 pub mod image_trans;
 
 use enigo::Enigo;
+use std::env;
 use std::time::Duration;
 
 fn main() {
+    let picture: String = env::args().nth(1).unwrap();
+
     // Assume that the game runs at 60Hz, we should refresh every 16ms. Hence,
     // let's wait two frames during each operation.
     let wait_time = Duration::from_millis(32);
     let mut enigo = Enigo::new();
     image_trans::draw_image(
-        "unicorn.jpg",
+        &picture,
         String::from("coords.json"),
         &mut enigo,
         &wait_time,
